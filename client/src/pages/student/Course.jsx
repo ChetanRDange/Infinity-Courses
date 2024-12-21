c3d8223be8fa14e6cbc76265db951236fd4ba82b
Chetan Dange <dangechetan3@gamil.com>
2024-12-21 16:57:38 +0530

    twelfth-commit

--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 const Course = ({ course }) => {
     return (
-        <Link to={`/course-detail/${course._id}`}>
+        <Link to={`/course-detail/${course?._id}`}>
             <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
                 <div className="relative">
                     <img
@@ -21,7 +21,7 @@ const Course = ({ course }) => {
                 </div>
                 <CardContent className="px-5 py-4 space-y-3">
                     <h1 className="hover:underline font-bold text-lg truncate">
-                        {course.courseTitle}
+                        {course?.courseTitle}
                      </h1>
                     <div className="flex items-center justify-between">
                         <div className="flex items-center gap-3">
@@ -32,11 +32,11 @@ const Course = ({ course }) => {
                             <h1 className="font-medium text-sm">{course.creator?.name}</h1>
                         </div>
                         <Badge className={'bg-blue-600 text-white px-2 py-1 text-xs rounded-full'}>
-                            {course.courseLevel}
+                            {course?.courseLevel}
                         </Badge>
                     </div>
                     <div className="text-lg font-bold">
-                        <span>₹{course.coursePrice}</span>
+                        <span>₹{course?.coursePrice}</span>
                     </div>
 
                 </CardContent>
--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const PORT = process.env.PORT || 3000;
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors({
+//   origin:`http://localhost:5173`,
+//   credentials: true
+// }))
 app.use(cors({
   origin:`https://infinity-courses-frontend.onrender.com`,
   credentials: true
