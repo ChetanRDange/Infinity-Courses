1887fe3ab510976e28866c45281f6288fd97ffba
Chetan Dange <dangechetan3@gamil.com>
2024-12-21 15:15:59 +0530

    second-commit

--- server/routes/purchaseCourse.route.js
+++ server/routes/purchaseCourse.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
+import isAuthenticated from "../Middlewares/isAuthenticated.js";
 import {
   createCheckoutSession,
   getAllPurchasedCourse,
